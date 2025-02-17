dbd8bd4e5691508a300137f29855b1a077bb32f5
Dmitry <d@kozlov.ru>
2025-02-17 16:51:04 +0700

    Added processing tests

--- Tests/FYVideoCompressorTests/ProcessingTests.swift
+++ Tests/FYVideoCompressorTests/ProcessingTests.swift
@@ -5,4 +5,20 @@
 //  Created by Unicorn on 23.01.24.
 //
 
-import Foundation
+import XCTest
+@testable import FYVideoCompressor
+import AVFoundation
+import VideoToolbox
+
+let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+final class ProcessingTests: XCTestCase {
+  @available (iOS 13.0, *)
+  func testCompressVideoWithScale() async throws {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "h-mm-ss"
+    let video = documents.appendingPathComponent("input.mp4")
+    let target = documents.appendingPathComponent("output-\(formatter.string(from: Date())).mp4")
+    let output = try await video.compressVideo(.h264())
+    try FileManager.default.moveItem(at: output, to: target)
+  }
+}
